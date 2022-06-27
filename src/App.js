2a6e9f5c7d147e3b10dc951d8bbe999dd33fc641
Ricardo Momoi <shinjimomoi@gmail.com>
2022-06-27 23:54:32 +0900

    add implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import './App.css';
+import {useState, useEffect} from "react";
+import { db } from "./firebase-config";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 
 function App() {
 
-  console.log(process.env)
-  return (
-    <div className="App">
+  const [newName, setNewName] = useState("")
+  const [newAge, setNewAge] = useState(0)
+  const [newImg, setNewImg] = useState("")
+  const [users, setUsers] = useState([]);;
+  const usersCollectionsRef = collection(db, "users");
 
+  const createUser = async () => {
+    await addDoc(usersCollectionsRef, {name: newName, age: Number(newAge), img: newImg});
+  }
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionsRef)
+      setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    };
+
+    getUsers();
+  }, []);
+
+  return (
+
+
+    <div className="App">
+      <input type="text" onChange={(event) => {setNewName(event.target.value)}} name='name' placeholder='Name' />
+      <input type="number" onChange={(event) => {setNewAge(event.target.value)}} name='age' placeholder='Age' />
+      <input type="text" onChange={(event) => {setNewImg(event.target.value)}} name='img' placeholder='Photo' />
+      <button onClick={createUser}>Create User</button>
+      {users.map((user) => {
+        return (
+          <div>
+            <h1>Name: {user.name}</h1>
+            <h1>Age: {user.age}</h1>
+            <img src={user.img} alt="" />
+          </div>
+        )
+      })}
     </div>
   );
 }
